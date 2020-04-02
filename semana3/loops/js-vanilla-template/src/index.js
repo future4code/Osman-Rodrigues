f8bc4ad628ad5f206b905de8f36b0ec5e5762c14
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-01 22:46:47 -0300

    Atualizacao1: Exercicios e Desafios, semana 3, aula 11

--- semana3/loops/js-vanilla-template/src/index.js
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -1,3 +1,4 @@
+//
 //Exercícios de interpretação de código
 /*1.
 O código executa a soma do valor da let sum ao seu próprio valor
@@ -47,3 +48,61 @@ let array4 = []
 for(let i=0; i<array.length; i++){
     array4.push('O elemento do index '+i+' é '+array[i]+('\n'))
 }console.log(array4)
+//
+//Desafios
+//1.
+/* 
+'0'
+'00'
+'000'
+'0000' 
+*/
+//2.
+
+const start = Number(prompt('Abracadabra! Você está no Jogo do Bruxo.'+('\n')+
+'Vamos jogar!'+('\n')+
+'Jogador 1, digite um número aleatório:'))
+let answer = Number(prompt('Agora, Jogador 2'+('\n')+'Chute um número até acertar:'))
+let tries = 1
+
+while(answer != start){
+    
+    const kicks = 'O número chutado foooi: ' + answer+('\n');
+    const dibre = 'Dibrou o Acerto!'+('\n');
+    if(answer > start){
+        const errouPaMais = kicks+dibre+'O número chutado é maior. Chute um menor.'
+        tries ++
+        answer = Number(prompt(errouPaMais+('\n')+'Jogador 2'+('\n')+'Chute um número até acertar:'))
+    }else{
+        const errouPaMenos = kicks+dibre+'O número chutado é menor. Chute um maior.'
+        tries++
+        answer = Number(prompt(errouPaMenos+('\n')+'Jogador 2'+('\n')+'Chute um número até acertar:'))
+    }
+} console.log('Acertooou! Tentativas até a mágica acontecer: '+tries)
+
+//3.
+
+const rdStart = Math.floor((Math.random() * 100) + 1)
+let answer = Number(prompt('Abracadabra! Você está no Jogo do Bruxo.'+('\n')+
+'Vamos jogar!'+('\n')+'Chute um número entre 1 e 100 até acertar:'))
+let tries = 1
+
+while(answer != rdStart){
+    
+    const kicks = 'O número chutado foooi: ' + answer+('\n');
+    const dibre = 'Você dibrou o acerto!'+('\n');
+    if(answer > rdStart){
+        const errouPaMais = kicks+dibre+'O número chutado é maior. Chute pamenos!'
+        tries ++
+        answer = Number(prompt(errouPaMais+('\n')+'Chute um número até acertar:'))
+    }else{
+        const errouPaMenos = kicks+dibre+'O número chutado é menor. Chute pamais!'
+        tries++
+        answer = Number(prompt(errouPaMenos+('\n')+'Chute um número até acertar:'))
+    }
+} console.log('É Goooool! Você foi abençoado pelo Ronaldinho Gaúcho da Sorte e por isso acertou!'+
+('\n')+'Tentativas até a mágica acontecer: '+tries)
+
+//Comentário sobre o código acima: aplicar a alteração foi fácil, porém chegar ao entendimento
+//de como ela funciona e pudesse ser aplicada exige um pouco de atenção. Não sei o que poderia
+//simplificar mais como foi aplicado acima. 
