829c15aa74d7cf5c3ff20b7064a6e1d2965a55e9
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-05 16:53:58 -0300

    Atualizacao2: Projeto Completo + Desafio

--- semana3/Blackjack/desafio.js
+++ semana3/Blackjack/desafio.js
@@ -12,45 +12,100 @@
  */
 console.log('Bem vindo ao jogo de Blackjack d@s Parcêr@!'+('\n')
 +'Para começar, clique em "Iniciar Rodada".');
-function startGame(){
+
+//Function que é acionada pelo 'onclick' do botão startH e da início ao jogo de Blackjack
+function startHardGame(){
    
-   const startU = confirm('Vamo iniciar uma nova rodada?'+('\n')+('\n'))
-   let cUser = '';
-   let cBot = '';
+   const startU = confirm('Vamo iniciar uma nova rodada?'+('\n')+('\n'))//Inicia a cadeia de validacao que estrutura o flow do jogo
    let sbut = startU;
-
+   //Bloco de loop while que valida o start do jogo
    while(sbut){
-      buser1 = comprarCarta(); buser2 = comprarCarta(); buser3= comprarCarta();
-      let cUser = [' '+buser1.texto,' '+buser2.texto,' '+buser3.texto]
-      let pUser = buser1.valor+buser2.valor+buser3.valor
-      bbot1 = comprarCarta(); bbot2 = comprarCarta(); bbot3 = comprarCarta();
-      let cBot = [' '+bbot1.texto, ' '+bbot2.texto, ' '+bbot3.texto]
-      let pBot = bbot1.valor+bbot2.valor+bbot3.valor
-      
-      uAce = 'teste1' //buser.texto[0]
-      bAce = 'teste2'//bbot.texto[0]
 
-      const mBlack = 'Suas duas primeiras cartas são: '+cUser[0]+' e '+cUser[1]+
-      '\n'+'A primeira carta do computador foi revelada: '+ cBot[0]+
+      //Escopo de definição da mão inicial
+      let stCardUser = comprarCarta(); 
+      let ndCardUser = comprarCarta(); //2ª(2nd) carta inicial do jogador
+      let stCardBot = comprarCarta(); //1ª (1st) carta inicial do computador ou robo(bot)
+      let ndCardBot = comprarCarta();
+
+      //Escopo de armazenamento de informações
+      let cUser = [stCardUser.texto, ndCardUser.texto]; //Array de cartas do jogador 
+      let pUser = stCardUser.valor+ndCardUser.valor ; //Array de pontuação do jogador
+      let cBot = [stCardBot.texto, ndCardBot.texto] ;
+      let pBot = stCardBot.valor + ndCardBot.valor ;
+      
+      //Escopo de armazenamento do evento 'Dois Ases na mão inicial'
+      uAces = cUser[0][0] + cUser[1][0];
+      bAces = cBot[0][0] + cBot[1][0];
+
+      //Escopo de armazenamento de mensagens para confirmação e/ou alertas
+      let mBlack = 'Suas cartas são: '+cUser+
+      '\n'+'A carta revelada do computador é: '+ cBot[Math.round(Math.random()*1)]+
       "\n"+ 
-      "Deseja comprar mais uma carta?";
-      const mUser = 'Suas cartas: '+cUser+' - pontuação '+pUser;
-      const mBot = 'Cartas do Computador: '+cBot+' - pontuação '+pBot;
-      const mAll = mUser+('\n')+mBot+('\n')
-      
+      "Deseja comprar mais uma carta?"; //Msg de mostra da mão inicial, com revelação de uma carta aleatória do bot
 
-      const jack = confirm(mBlack)
-      if(jack){
-         if(pUser>pBot){
-            confirm(mAll+'Você ganhou, parcêr@!!!')
-         }else if(bbot1.valor===11 && buser1.valor===11){
-            sbut = confirm(mAll+'Opa! Não pode haver empate entre Dois Ases. Tente novamente!')
+      function bjAlert(){
+         let mUser = 'Suas cartas: '+cUser+' - pontuação '+pUser;
+         let mBot = 'Cartas do Computador: '+cBot+' - pontuação '+pBot;
+         let mAll = mUser+('\n')+mBot+('\n');
+         return mAll
+      } //Function de apresentação do resultado da rodada
+
+      //Bloco de compra de cartas extras mediante validação de confirms e de decisão do resultado
+      let jack = confirm(mBlack);
+      if(jack && pUser <= 21){
+         //Escopo de compra do jogador
+         while(jack && pUser <= 21){
+            bcard = comprarCarta();         
+            pUser += bcard.valor;
+            cUser.push(bcard.texto);
+            mBlack = 'Carta comprada: '+bcard.texto+
+                  "\n"+
+                  'Suas cartas são: '+cUser+'\n';
+            if(pUser < 21){
+               jack = confirm(mBlack+'Deseja comprar mais uma carta?')
+            }else{
+               alert(mBlack+'Pontuação Máxima (21) atingida ou ultrapassada! Não é permitido comprar mais cartas.')
+               jack = false
+            }      
+         }
+         //Escopo de compra do bot
+         while(pBot <= pUser){
+            bcard = comprarCarta();         
+            pBot += bcard.valor;
+            cBot.push(bcard.texto);     
+         }
+         //Escopo de validação das pontuações e decisão dos resultados caso o jogador tenha optado por comprar mais cartas
+         if(pUser > 21 || pBot <= 21 && pUser < pBot){
+            alert(bjAlert()+'Tu perdeu, parcêr@...');
+         }else if(uAces === 'AA' || bAces === 'AA'){
+            alert(bjAlert()+'Opa! Dois Ases de início não é permitido. Vamos embaralhar e recomeçar a rodada!')
          }else if(pUser===pBot){
-            confirm(mAll+'Empate, parcêr@!')
-         }else{
-            confirm(mAll+'Tu perdeu, parcêr@...')
-         } sbut = confirm('Gostaria de tentar novamente?'+('\n')+('\n'));
-      }else{
+            alert(bjAlert()+'Empate, parcêr@!')
+         }else if(pBot > 21){
+            alert(bjAlert()+'Você ganhou, parcêr@!!!')
+         }
+         else{
+            alert(bjAlert()+'Você ganhou, parcêr@!!!')
+         }
+         sbut = confirm('Jogar outra rodada?'+('\n')+('\n'));//Inicia nova rodada
       }
-   }confirm('Aaaah...Que pena! Valeu e até a próxima!'+('\n')+('\n'))
+      
+      else{
+         //Escopo caso usuário tenha optado por manter a mão inicial
+         if(pUser > 21 || pBot <= 21 && pUser < pBot){
+            alert(bjAlert()+'Tu perdeu, parcêr@...');
+         }else if(uAces === 'AA' || bAces === 'AA'){
+            alert(bjAlert()+'Opa! Dois Ases de início não é permitido. Vamos embaralhar e recomeçar a rodada!')
+         }else if(pUser === pBot){
+            alert(bjAlert()+'Empate, parcêr@!')
+         }else if(pBot > 21){
+            alert(bjAlert()+'Você ganhou, parcêr@!!!')
+         }
+         else{
+            alert(bjAlert()+'Você ganhou, parcêr@!!!')
+         } 
+         sbut = confirm('Jogar outra rodada?'+('\n')+('\n'));//Inicia nova rodada
+      }
+   }
+   alert('Aaaah...Que pena! Valeu e até a próxima!'+('\n')+('\n'))//Encerra o jogo
 }
