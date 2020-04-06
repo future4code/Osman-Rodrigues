81ef2815db6c0eb7cd2b1f3b850c8e9efb10d56f
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-06 19:56:55 -0300

    Exercicios semana 4, aula 13 (todos completos)

--- semana4/funcoes/js-vanilla-template/src/index.js
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -18,13 +18,13 @@
 //
 //4.
 //a.
-let euSouCaxorruNao = (tempoDeVida) =>{
+let verIdadeDeCatioro = (tempoDeVida) =>{
     idadeDeCatioro = tempoDeVida*7;
     return idadeDeCatioro
 }   
 
 //b.
-let infosPessoais = (nome, idade, endereço, estuda) => {
+let emitirInfosPessoais = (nome, idade, endereço, estuda) => {
     
     if(estuda === true){
         let estudante = 'sou';
@@ -38,20 +38,22 @@ let infosPessoais = (nome, idade, endereço, estuda) => {
     return infos
 }
 //5.
-let meDigaOSec = (ano) =>{
+let verOSeculo = (ano) =>{
     let romanos = ['I', 'V', 'X'];
     let doisUltimos = String(ano)[2] + String(ano)[3];
     let ultimosConvert = Number(doisUltimos);
     let doisIniciais = String(ano)[0] + String(ano)[1];
     let iniciaisConvert = Number(doisIniciais);
+    console.log(doisUltimos)
+    console.log(doisIniciais)
     
     let seculo = 0;
 
     if(ultimosConvert > 0){
-        seculo += iniciaisConvert;
+        seculo += iniciaisConvert+1;
         seculo = [seculo];
     }else{
-        seculo = iniciaisConvert + 1;
+        seculo = iniciaisConvert;
         seculo = [seculo]; 
     }
 
@@ -61,13 +63,102 @@ let meDigaOSec = (ano) =>{
         numero1N = Number(numero1)
         numero2N = Number(numero2)
 
-        if(Number(numero1)<4){
-            switch()
-
+        if(numero1N<3){
+            console.log('passou 0')
+            switch(numero1N){
+                case 1:
+                    numero1 = romanos[2]
+                    break;
+                case 2:
+                    numero1 = romanos[2]+romanos[2]
+                    break
+                default:
+                    numero1 = romanos[2]
+                    break
+            }
+            if(numero2N<4){
+                console.log('passou 1')
+                switch(numero2N){
+                    case 1:
+                        numero2 = romanos[0]
+                        break;
+                    case 2:
+                        numero2 = romanos[0]+romanos[0]
+                        break;
+                    case 3:
+                        numero2 = romanos[0]+romanos[0]+romanos[0]
+                        break;
+                    default:
+                        numero2 = ''
+                        break;
+                }
+            }else if(numero2N === 4){
+                console.log('passou 2')
+                numero2 = romanos[0]+romanos[1]
+            }else if(numero2N === 5){
+                console.log('passou 3')
+                numero2 = romanos[1]
+            }else if(numero2N>5){
+                console.log('passou 4')
+                switch(numero2N){
+                    case 6:
+                        numero2 = romanos[1]+romanos[0]
+                        break;
+                    case 7:
+                        numero2 = romanos[1]+romanos[0]+romanos[0]
+                        break;
+                    case 8:
+                        numero2 = romanos[1]+romanos[0]+romanos[0]+romanos[0]
+                        break;
+                    case 9:
+                        numero2 = romanos[0]+romanos[2]
+                        break;
+                    default:
+                        numero2 = ''
+                        break;
+                }
+            }
+        seculo = [numero1+numero2]
         }
     }
-
-
-
     
+    return 'O ano '+ano+' pertence ao século '+seculo[0]+'.';
 }
+//6.
+const array1 = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+//a.
+let tamanhoDoArray = (array) =>{
+    return 'Quantidade de elementos neste array: '+array.length
+} 
+//b.
+let esteNumEPar = (numero) => {
+    if(numero !== undefined){
+        if(numero%2===0){
+            return true
+        }else{
+            return false
+        }
+    }    
+}
+//c.
+let saberOsPares = (array) =>{
+    const pares =[]
+    for(let numero of array){
+        let par = numero%2;
+        if(par===0){
+            pares.push(par)
+        }
+    }
+    return 'Este array possui '+pares.length+' números pares.'
+}
+//d.
+let saberOsPares2 = (array) =>{
+    const pares =[]
+    for(let numero of array){
+        if(esteNumEPar(numero)){
+            pares.push(numero)
+        }
+    }
+    return 'Este array possui '+pares.length+' números pares.'
+} 
