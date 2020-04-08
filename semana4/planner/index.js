5d74ba5b483d6db501501d2bc641c23178899259
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-07 23:58:58 -0300

    Atualizacao1: Desafio completo, exceto item da responsabilidade mobile

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,37 +1,61 @@
 /*Input funcionalities*/
 const daysList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
+let task = document.getElementById('taskCatcher');
+let hour = document.getElementById('hourCatcher').value;
+let taskIndex = 1;
 let creatTask = () =>{
-    let task = document.getElementById('taskCatcher');
+    let hour = document.getElementById('hourCatcher').value;
     let day = document.getElementById('daySelector')
-    
-    switch(day.value){
-        case daysList[0]:
-            document.getElementById('mondayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
-        case daysList[1]:
-            document.getElementById('tuesdayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
-        case daysList[2]:
-            document.getElementById('wednesdayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
-        case daysList[3]:
-            document.getElementById('thursdayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
-        case daysList[4]:
-            document.getElementById('fridayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
-        case daysList[5]:
-            document.getElementById('saturdayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
-        case daysList[6]:
-            document.getElementById('sundayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
-        default:
-            document.getElementById('mondayBox').innerHTML += "<p>"+task.value+"</p>"
-            break;
+    if((task.value).trim() === ''){
+        alert('Insira algum texto!')
+    }else{
+        
+        switch(day.value){
+            case daysList[0]:
+                document.getElementById('mondayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+")'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+            case daysList[1]:
+                document.getElementById('tuesdayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+"h)'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+            case daysList[2]:
+                document.getElementById('wednesdayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+"h)'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+            case daysList[3]:
+                document.getElementById('thursdayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+"h)'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+            case daysList[4]:
+                document.getElementById('fridayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+"h)'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+            case daysList[5]:
+                document.getElementById('saturdayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+"h)'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+            case daysList[6]:
+                document.getElementById('sundayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+"h)'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+            default:
+                document.getElementById('mondayBox').innerHTML += "<p class='task' id='task"+taskIndex+"' onclick='lineThrough("+taskIndex+"h)'>"+task.value+"("+hour+"h)"+"</p>"
+                taskIndex ++
+                break;
+        }
     }
     task.value = '';
+    hour = "";
+}
+
+let lineThrough =(indexOfTask) => {
+    let modifTask = document.getElementById("task"+indexOfTask+"");
+    modifTask.style.textDecoration = "line-through";
+}
+let clearTasks = (idDayBox)  =>{
+    let tasksContainer = document.getElementById(idDayBox);
+    tasksContainer.innerHTML = ""   
 }
 
 
