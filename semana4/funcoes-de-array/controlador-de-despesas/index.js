de5220becf448399fba61b0c279a8c46a8d4da3a
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-09 16:56:51 -0300

    Exercicio semana 4, aula 16

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,15 @@
+var expCount = 0;
+const expCont = [];
+const expImg = {
+    id:'',
+    value: 0,
+    type: '',
+    desc:''
+};
+
+const expGen = () =>{
+    let expId = 'expId'+(expCount+1);
+    let expValue = document.getElementById('expValue');
+    let expType = document.getElementById('expType');
+    let expDesc = document.getElementById('expDesc');
+}
