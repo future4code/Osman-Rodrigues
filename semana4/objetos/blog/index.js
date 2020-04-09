506d0531260da2c33877b9b26496c62eb6f52e74
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-08 23:59:28 -0300

    Atualizacao2: Exercicios + Desafio (até o 1º item)

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,4 +1,5 @@
-let postsCount = 1;
+let postsCount = 0;
+const postsArray = [];
 const postsContainer = [];
 
 const generatePost = () =>{
@@ -7,31 +8,49 @@ const generatePost = () =>{
         id:'',
         userName:'',
         postTitle:'',
-        postContent:''
+        postContent:'',
+        postImage:''
     };
     
     let user = document.getElementById('userName');
     let title = document.getElementById('postTitle');
     let content = document.getElementById('postContent');
+    let image = document.getElementById('postImage');
     let timeLine = document.getElementById('timeLine');
+    
+    
 
-    const postParts = ['<article id="', '">', '</article>', '<h2>', '</h2>', '<p>', '</p>', '<span> Autor: ','</span>'];
+    const postParts = ['<article id="', '">', '</article>', '<h2>', '</h2>',
+     '<p>', '</p>', '<span> Autor: ','</span>','<img src="','" alt="','_img">'];
 
 
-    userPost.id += 'userPost'+postsCount;
+    userPost.id += 'user_Post_'+postsCount+1;
     userPost.userName += user.value;
     userPost.postTitle += title.value;
     userPost.postContent += content.value;
 
-    postsContainer.push(userPost)
+    if(
+        image.value.includes(".jpeg") ||
+        image.value.includes(".jpg") ||
+        image.value.includes(".png")
+    ){
+        userPost.postImage += image.value;
+    }
+    else{
+        alert('Endereço URL não suportado. Insira um URL de imagem válido!');
+    }
+
+    postsArray.push(userPost)
 
     user.value ='';
     title.value ='';
     content.value ='';
-    postsCount++;
+    image.value ='';
 
-    timeLine.innerHTML += 
+    const postMountString = 
     postParts[0] + userPost.id + postParts[1]+
+
+    postParts[9]+userPost.postImage+postParts[10]+userPost.id+postParts[11]+
     
     postParts[3]+userPost.postTitle+postParts[4]+
 
@@ -41,7 +60,13 @@ const generatePost = () =>{
     
     postParts[2];
 
+    postsContainer.push(postMountString);
+
+    timeLine.innerHTML += postsContainer[postsCount];
+    postsCount++;
+
 }
+
 
 
 
