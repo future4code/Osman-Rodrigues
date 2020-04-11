01d9c5cc696e0036a728968aaba7e1e7df7aa133
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-11 10:56:15 -0300

    Resolucao (completa) dosExercicios de leitura de codigo

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,49 @@
+//
+//1ª Parte do Projeto, da semana 4
+// 
+/* Exercícios de leitura de código
+
+1.
+O que o código faz: a conversão de R$ para US$ por uma função que recebe como parametro o valor em R$; 
+
+Como faz: a partir do input de cotação do US$ (solicitado ao usuário), a função retorna o resultado da multiplicação
+entre essa cotação e o valor recebido como parametro;
+
+Valor que será impresso no console: 
+R$100*cotação em US$ . 
+
+2.
+O que o código faz: calcula o montante de diferentes tipos de investimentos mediante um valor estipulado;
+
+Como faz: a partir de uma função que recebe como parametros o tipo de investimento desejado e o valor aplicado,
+o tipo repassado passa por validação em condicional switch case que atribui taxa relativa a cada modalidade, sendo
+esta multiplicada ao valor aplicado. Caso não tenha sido inserido um tipo de investimento previsto, é rerotnado alerta,
+caso inserido, é retornado o valor decorrente da multiplicação valor pela taxa;
+
+Valor que será impresso no console: 
+165
+'TIPO DE INVESTIMENTO INFORMADO INCORRETO!' . 
+
+3.
+O que o código faz: cria dois arrays diferentes de números a partir de um terceiro array;
+
+Como faz: após declaração dos três arrays (um de referencia e dois vazios), é aplicado um loop for of para iterar 
+o array de referencia sendo separados os números pares e impares mediante validação do condicional if else, sendo
+o número verificado adicionado pelo metodo .push(), caso seja par, adiconado ao array1, caso impar ao array2;
+
+Valor que será impresso no console: 
+'Quantidade total de números 14'
+6
+7 .
+
+4.
+O que o código faz: atribui valores correspondentes aos itens de um array de números à duas variaveis pré-definidas;
+
+Como faz: a partir de um loop for of, responsável por iterar o array numeros, o qual valida por meio do condicional
+if else se o valor do item é maior ou menor que o valor de cada variavel. Caso menor que o valor da variavel numero1,
+o valor do item será atribuido a mesma, caso maior que o valor da variavel numero2, será atribuido a esta;
+
+Valor que será impresso no console:
+1590
+1590 . */
+//
