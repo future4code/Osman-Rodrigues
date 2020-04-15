570f572e5fcd09ea18605c0b7fcb06002140eb64
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-04-15 16:22:57 -0300

    Exercicios semana 5, aula 19 - React Insta4

---
+++ semana5/componentes-de-classe/insta4/src/components/Post/Post.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import './Post.css'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
+class Post extends React.Component {
+  state = {
+    curtido: false,
+    numeroCurtidas: 0,
+    comentado: false,
+    numeroComentarios: 0
+  }
+
+  onClickCurtida = () => {
+    if (this.state.numeroCurtidas === 0){
+      this.setState({
+        curtido: !this.state.curtido,
+        numeroCurtidas: this.state.numeroCurtidas + 1
+      })
+    }else{
+      this.setState({
+        curtido: !this.state.curtido,
+        numeroCurtidas: this.state.numeroCurtidas - 1
+      })
+    }
+  }
+
+  onClickComentario = () => {
+    this.setState({
+      comentado: !this.state.comentado
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentado: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(this.state.comentado) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    }
+
+    return <div className={'post-container'}>
+      <div className={'post-header'}>
+        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </div>
+
+      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
+
+      <div className={'post-footer'}>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </div>
+      {componenteComentario}
+    </div>
+  }
+}
+
+export default Post
