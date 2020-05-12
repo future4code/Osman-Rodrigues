8faf1664777693b532c100966e9471765a8fc94c
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-05-12 17:46:34 -0300

    Criação do App React

---
+++ semana9/hooks-useeffect/pokeapi-useeffect/build/precache-manifest.368adfe90aa9d51f8ccea82dadb11ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a59d349e8b10ed4278f2d457bb744f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f28189956c0b20e5528",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "b7bcd8d91b39fba8c2b9",
+    "url": "/static/js/2.5c337b7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5c337b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f28189956c0b20e5528",
+    "url": "/static/js/main.32f7abc0.chunk.js"
+  },
+  {
+    "revision": "dce4af1269a9d6764249",
+    "url": "/static/js/runtime-main.d170e735.js"
+  }
+]);
