e837b5737952ca19947a5424b28a4aaf3dcfddeb
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-06-22 16:59:45 -0300

    inicialização dos exercicios da aula 44

---
+++ semana14/exercicios-aula44/src/exercicios/exercicio1.ts
@@ -0,0 +1,23 @@
+//a) 
+let minhaString: string = 'minha string';
+minhaString = 0;
+//r. o compilador não permite que uma variável tipada como string tenha atribuição de tipo number 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
