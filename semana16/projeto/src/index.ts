1432cef0dd8e9c7471310b8155658ecc64fab7b7
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-07-12 12:32:39 -0300

    finalização dos endpoints 2, 3, 4 e 5

--- semana16/projeto/src/index.ts
+++ semana16/projeto/src/index.ts
@@ -6,9 +6,11 @@ import {AddressInfo} from 'net';
 import axios from 'axios';
 
 import * as crypto from 'crypto';
+import * as moment from 'moment';
 
-//Getting .env variables
+//Getting environment variables
 dotenv.config();
+moment.locale();
 
 //Setting DB connection
 const connection = knex({
@@ -71,18 +73,19 @@ const createTasksTable = async (): Promise<void> =>{
 };
 
 //Endpoints
+//1. Create user
 app.put('/user', async (req: express.Request, res: express.Response): Promise<any>=>{
   const body = req.body;
   const id = crypto.randomBytes(6).toString('hex');
 
   if(! body.name || body.name.trim() === ''){
-    res.send({message: 'Username missed.'}).status(400)
+    res.send({message: 'Missing username.'}).status(400)
     return
   }else if(! body.email || body.email.trim() === ''){
-    res.send({message: 'E-mail missed.'}).status(400)
+    res.send({message: 'Missing e-mail.'}).status(400)
     return
   }else if(! body.nickname || body.nickname.trim() === ''){
-    res.send({message: 'Nickname missed.'}).status(400)
+    res.send({message: 'Missing nickname.'}).status(400)
     return
   };
   
@@ -103,6 +106,195 @@ app.put('/user', async (req: express.Request, res: express.Response): Promise<an
   };
 });
 
+//2. Get user
+app.get('/user/:id', async (req: express.Request, res: express.Response)=>{
+  const userId = req.params.id;
+  try{
+    const r = await connection('ToDoUsers')
+    .select('*')
+    .where('id', '=', userId);
+
+    res.send(r[0]).status(200);
+  }catch(e){
+    res.send({
+      message: e.message,
+    })
+    .status(400)
+  };
+});
+
+//3. Edit user
+app.post('/user/edit/:id', async (req: express.Request, res: express.Response)=>{
+  const userId = req.params.id;
+  const body = req.body;
+
+  if(! userId){
+    res.send({message: 'Missing user id.'}).status(400)
+    return {message:''}
+  };
+
+  switch(body){
+    case body.name && ! body.nickname:
+      try{
+        await connection('ToDoUsers')
+        .where('id', '=', userId)
+        .update({
+          name: body.name
+        });
+
+        res.sendStatus(200);
+        break
+      }catch(e){
+        res.send({
+          message: e.message
+        })
+        .status(400);
+        break
+      };
+    case ! body.name && body.nickname:
+      try{
+        await connection('ToDoUsers')
+        .where('id', '=', userId)
+        .update({
+          nickname: body.nickname
+        });
+
+        res.sendStatus(200);
+        break
+      }catch(e){
+        res.send({
+          message: e.message
+        })
+        .status(400);
+        break
+      };
+    case ! body.name && ! body.nickname:
+      res.send({
+        message: 'Inform name or nickname to update.'
+      }).status(400);
+    default:
+      try{
+        await connection('ToDoUsers')
+        .where('id', '=', userId)
+        .update({
+          name: body.name,
+          nickname: body.nickname
+        });
+
+        res.sendStatus(200);
+        break
+      }catch(e){
+        res.send({
+          message: e.message
+        })
+        .status(400);
+        break
+      };
+  };
+});
+
+//4. Create task 
+app.put('/task', async (req: express.Request, res: express.Response): Promise<any>=>{
+  const body = req.body;
+  const id = crypto.randomBytes(4).toString('hex');
+
+  if(! body.title || body.title.trim() === ''){
+    res.send({message: 'Missing task title.'}).status(400)
+    return
+  }else if(! body.limitDate || body.limitDate.trim() === ''){
+    res.send({message: 'Missing task limit date.'}).status(400)
+    return
+  }else if(! body.creatorId || body.creatorId.trim() === ''){
+    res.send({message: 'Missing task creator id.'}).status(400)
+    return
+  };
+  
+  try{
+    await connection('ToDoTasks')
+    .insert({
+      id,
+      title: body.title,
+      description: body.description,
+      limit_date: moment(body.limitDate, 'DD/MM/YYYY').format('YYYY-MM-DD'),
+      creator_id: body.creatorId
+    });
+    res.sendStatus(200);
+  }catch(e){
+    res.send({
+      message: e.message,
+    })
+    .status(400);
+  };
+});
+
+//5. Get task
+app.get('/task/:id', async (req: express.Request, res: express.Response)=>{
+  const taskId = req.params.id;
+  try{
+    const r = await connection('ToDoTasks')
+    .select('*')
+    .where('id', '=', taskId);
+
+    res.send(r[0]).status(200);
+  }catch(e){
+    res.send({
+      message: e.message,
+    })
+    .status(400)
+  };
+});
+
+//Tests
+const getTaskById = async (taskId: string): Promise<any> =>{
+  try{
+    const r = await axios.get(`http://localhost:3003/task/${taskId}`);
+    console.log(r.data);
+  }catch(e){
+    console.log(e.data.message)
+  }
+};
+
+const newTask = async (
+  creatorId: string, title: string, limitDate: string, description?: string,
+): Promise<void> =>{
+  try{
+    const r = await axios.put('http://localhost:3003/task',{
+      title,
+      description,
+      limitDate,
+      creatorId
+    });
+    console.log(r.data);
+  }catch(e){
+    console.log(e.data.message);
+  };
+};
+
+const updateUserInfos = async (
+  userId: string, name?: string, nickname?: string
+  ): Promise<any> =>{
+    const body = {
+      name,
+      nickname
+    };
+
+    try{
+      const r = await axios.post(`http://localhost:3003/user/edit/${userId}`, body);
+      console.log(r.data);
+    }catch(e){
+      console.log(e.data.message);
+    };
+};
+
+const getUserById = async (userId: string): Promise<any> =>{
+  try{
+    const r = await axios.get(`http://localhost:3003/user/${userId}`);
+    console.log(r.data);
+  }catch(e){
+    console.log(e.data.message)
+  }
+};
+
 const newUser = async (
   name: string, nickname: string, email: string
 ): Promise<void> =>{
@@ -117,11 +309,3 @@ const newUser = async (
     console.log(e.data.message);
   };
 };
-
-newUser('Marie Huana', 'Marie', 'marie@huana.com');
-
-//createTasksTable();
-//createUserTable();
-
-//const test = crypto.randomBytes(12).toString('hex');
-//console.log(test)
